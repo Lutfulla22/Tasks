a1b917bc695048a3495139b781060833b3d79266
Lutfulla22 <nlutfulla2112@mail.ru>
2021-11-11 02:37:47 +0500

    Inject storage

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,7 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using tasks.services;
+
 namespace Tasks.Controllers
 {
-    public class TaskController
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
     {
-        
+        private readonly ILogger<TaskController> _logger;
+        private readonly IStorageService _storage;
+
+        public TaskController(ILogger<TaskController> logger, IStorageService storage)
+        {
+            _logger = logger;
+            _storage = storage;
+        }
     }
 }
